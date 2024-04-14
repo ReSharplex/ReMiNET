a631b6f1e4bfe0aad11f518182b203e348b23fd8
ReSharplex <166249324+ReSharplex@users.noreply.github.com>
2024-04-14 16:09:44 +0200

    added sample Plugin

---
+++ src/MiNET/MiNET/Plugins/Samples/SamplePlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiNET.Plugins.Attributes;
+using MiNET.Worlds;
+
+namespace MiNET.Plugins.Samples
+{
+	[Plugin]
+	internal class SamplePlugin : Plugin
+	{
+		[Command]
+		public void gm(Player player, GameMode gamemode)
+		{
+			player.SetGameMode(gamemode);
+		}
+	}
+}
